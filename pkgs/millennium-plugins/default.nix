fbc76c92ee1e32db1ce07c89a82cf79cce4da691
Rein <rein@rein.icu>
2026-04-26 12:00:24 +0200

    feat: Non-Steam Playtimes plugin

--- pkgs/millennium-plugins/default.nix
+++ pkgs/millennium-plugins/default.nix
@@ -17,9 +17,13 @@ let
   inherit (pins)
     Extendium
     HLTB
+    non-steam-playtimes
     ;
 in
 {
   extendium = mkPlugin "extendium" Extendium.version Extendium;
   hltb = mkPlugin "hltb-for-millennium" HLTB.version HLTB;
+  non-steam-playtimes =
+    mkPlugin "non-steam-playtimes" "0-git+${non-steam-playtimes.revision}"
+      non-steam-playtimes;
 }
